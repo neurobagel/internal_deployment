7f82b643f145cf6b48724fbd6f828e37b78e8b0d
Alyssa Dai <alyssa.ydai@gmail.com>
2025-01-21 12:38:50 -0500

    [MNT] Add logging of run details to symlink script (#4)
    * write run details to logfile
    
    * make script executable
    
    * hardcode location of log and lock files
    
    * add comments explaining log file purpose

--- helper_scripts/symlink_neurobagel_results_on_bic.sh
+++ helper_scripts/symlink_neurobagel_results_on_bic.sh
@@ -10,6 +10,9 @@ TARGET_DIR=$2
 SESSION_PATH_COL="ImagingSessionPath"
 SESSION_PATH_COL_IDX=6
 
+LOG_FILE="/data/pd/neurobagel/scripts/symlink_neurobagel_results_on_bic.log"
+LOCK_FILE="/data/pd/neurobagel/scripts/symlink_neurobagel_results_on_bic.lock"
+
 # Extract session path column and remove i) the column header, ii) any empty lines, iii) any "protected" lines, and iv) duplicates (just in case)
 session_paths=$(cut -d $'\t' -f $SESSION_PATH_COL_IDX $RESULTS_TSV | grep -v "${SESSION_PATH_COL}" | grep -v "^$" | grep -v "protected" | sort | uniq)
 
@@ -50,3 +53,18 @@ EOF
         fi
     fi
 done
+
+# If log file exists, write datetime and user ID of run to log file
+# This keeps a basic record of how many times and by how many unique users the script was run
+if [ -f $LOG_FILE ]; then
+    # Check if lock file exists (it should be pre-created, because not all users will have write permissions to the shared directory itself)
+    if [ -f $LOCK_FILE ]; then
+        # Open lockfile and associate it with arbitrary file descriptor 200 (specific to the context of this process/script)
+        exec 200>$LOCK_FILE
+        # Try to acquire the lock associated with the lockfile. 
+        # If the lockfile is already "locked", flock first waits until the lock is released
+        flock 200
+    fi
+    # Log that script was run (only date and user ID) for debugging and to get a sense of script usage
+    echo -e "Date run: $(date)\tUser ID: $(id -u)">> $LOG_FILE
+fi
